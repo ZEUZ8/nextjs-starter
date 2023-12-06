df0658fc2eeccf58dbf7aec8a866b47789d0179c
sinan <ptsinan8590@gmail.com>
2023-12-06 20:52:19 +0530

    mplemented basic login, signup, and user profile pages, prioritizing simplicity over design in line with the project's main goals.

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,19 @@
+import { error } from 'console';
+import mongoose from 'mongoose'
+
+export async function Connect(){
+    try{
+        mongoose.connect(process.env.MONGO_URL!);
+        const connection = mongoose.connection;
+        connection.on("connected",()=>{
+            console.log('MonogDB connected succesfull')
+        })
+        connection.on("error",()=>{
+            console.log('MongoDB connection erorr. please make sure MongoDB is running'+error)
+            process.exit()
+        })
+    }catch(err){
+        console.log("somtig wend wron")
+        console.log(err)
+    }
+}
